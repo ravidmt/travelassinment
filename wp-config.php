c95158bbea68a248899822dad6e4a216a4000a06
ravidmt <69569074+ravidmt@users.noreply.github.com>
2022-10-30 20:38:49 +0530

    freedb

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'travel' );
+define( 'DB_NAME', 'freedb__travel' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'freedb__travel' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'GSz6KKJUsmv6uG!' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
